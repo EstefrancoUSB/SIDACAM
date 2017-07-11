360a4f8e9ad3c893980b8abc7a12092b9de55e42
estefranco_USB <ramiro.franco@usbmed.edu.co>
2017-07-11 14:29:29 -0500

    Cambios en comentarios de codigo y chequeo general
    Se  hizo un cambio en el primer ciclo a nivel de comentario, se agregaron otros comentarios y se chequeo el codigo para un cambio mas grande. Se concidera que puede ser reducido a un solo ciclo y convertirse en funcion

--- MaximosDeBandas.m
+++ MaximosDeBandas.m
@@ -1,11 +1,11 @@
-% M√ÅXIMOS DE BANDAS.
+% M√?XIMOS DE BANDAS.
 % ---------------------------------------------------------------
 % Luis Alberto Tafur Jimenez, decano.ingenierias@usbmed.edu.co
 % Luis Esteban Gomez, estebang90@gmail.com
 % David Perez Zapata, b_hh@hotmail.es
 %
 
-% Codigo encargado de Grabar tanto la se√±al de la 
+% Codigo encargado de Grabar tanto la seÒal de la 
 % lancha como del ruido de fondo, obtener de cada
 % una su PSD y por medio de un banco de filtros
 % llegar a obtener los umbrales de deteccion.
@@ -15,7 +15,7 @@
 Dim_fft = 4096;  % Minima longitud de ventana para optima resolucion en fft
 Frec_Corte1 = 300;
 N_Frec = 35;
-Step = 50 ;         %Delta del banco de filtros, desviaciÔøΩn estÔøΩndar
+Step = 50 ;         %Delta del banco de filtros, desviacion estandar
 ID = 1;             %Indice de directividad (Omnidireccional)
 Max_Bandas_dB = zeros(1,N_Frec); %maximo de bandas en dB.                                                               
 Frec_Max_B = zeros(1,N_Frec);   %maximos de frecuencias en decibeles                                             
@@ -24,28 +24,30 @@ Nivel_Ruido_Vector_B = zeros(1,N_Frec);
 Rest_Umbral_B = zeros(1,N_Frec); 
 Nivel_S_B = zeros(1,N_Frec);  % Nivel senal vector B.
 
-% Cargando seÔøΩal a analizar en S_Blanco. 
+% Cargando seÒal a analizar en S_Blanco. 
 [S_Blanco, Frec_Muestreo] = audioread('R2-INT1.wav');
 [S_R_Fondo, ~] = audioread('RUIDO DE FONDO RECOR.wav');
 
 
-%% Llenado del vector de m?ximos.
+%% Llenado del vector de maximos.
 for i=1:N_Frec
     
     % Dise?o Filtro Pasa-Banda
     Orden_Filtro = 8;
-    Frec_Corte2 = Frec_Corte1 + Step;
+    Frec_Corte2 = Frec_Corte1 + Step;       % Donde el ancho de banda = Step
     Param_Filtro = fdesign.bandpass('N,F3dB1,F3dB2',...
         Orden_Filtro,Frec_Corte1,Frec_Corte2,Frec_Muestreo);
     Filtro = design(Param_Filtro,'butter');
-   
-    % Densidad Espectral de Potencia de la seÔøΩal filtrada
-     S_Blanco_Filtrada = filter(Filtro,S_Blanco);
+    
+    % Filtro de la seÒal
+    S_Blanco_Filtrada = filter(Filtro,S_Blanco);
+    
+    % Densidad Espectral de Potencia de la seÒal filtrada
     [pxx,Frecuencias]=pwelch(S_Blanco_Filtrada,...
         hamming(Dim_fft),[], [], Frec_Muestreo);
     pxxdB = 10*log10(pxx);
-        
-    % BÔøΩsqueda de los valores MÔøΩximos y su correspondiente frecuencia
+    
+    % Busqueda de los valores Maximos y su correspondiente frecuencia
     [Max_Bandas_dB(i),posicion] = max(pxxdB);
     Frec_Max_B(i) = Frecuencias(posicion);
     Frec_Corte1 = Frec_Corte2;
@@ -53,8 +55,8 @@ for i=1:N_Frec
 end
 
 Max_And_Frec_B = [Max_Bandas_dB;Frec_Max_B]; % Matriz, almacena
-            % las frequencies donde hay maximos y su valor.
-            
+% las frequencies donde hay maximos y su valor.
+
 
 %% Umbrales de detecci?n
 for i=1:N_Frec
